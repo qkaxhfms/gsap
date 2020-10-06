5d2939a1840259adea51e4612fd94bce2b3b8910
unknown <kwakms821@daum.net>
2020-10-07 00:45:07 +0900

    초기값 설정

--- scrollTrigger/sample2/scroll.js
+++ scrollTrigger/sample2/scroll.js
@@ -1,13 +1,15 @@
 // gsap
 
-gsap.to(".section-intro .inner",{
+gsap.from(".section-intro .inner",{
+    opacity:0,
+    duration:2,
     scrollTrigger:{
         trigger:".section-intro",
-        start:"center center",
+        start:"center bottom",// 앞에는 해당요소 뒤에는 뷰포트 크기
         // end:"top bottom",
         markers:{
-            startColor:"white",
-            endColor:"yellow",
+            startColor:"red",
+            endColor:"green",
         },
         id:"intro",
         
