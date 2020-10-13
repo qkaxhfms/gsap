77ba0c693a7dedb54aee21c888f8e18bd9a05630
unknown <kwakms821@daum.net>
2020-10-13 23:50:45 +0900

    scroll proxy

--- scrollTrigger/sample2/scroll.js
+++ scrollTrigger/sample2/scroll.js
@@ -6,7 +6,7 @@ gsap.from(".section-intro .inner",{
     scrollTrigger:{
         trigger:".section-intro",
         start:"center bottom",// 앞에는 해당요소 뒤에는 뷰포트 크기
-        // end:"top bottom",
+        end:"top bottom",
         markers:{
             startColor:"red",
             endColor:"green",
@@ -14,4 +14,21 @@ gsap.from(".section-intro .inner",{
         id:"intro",
         
     }
+});
+
+gsap.from(".section-discussion .inner",{
+    opacity:0,
+    scrub:true,
+    scrollTrigger:{
+        trigger:".section-discussion",
+        start:"center bottom",
+        end:"bottom bottom",
+
+        markers:{
+            startColor:"blue",
+            endColor:"black",
+        },
+        id:"intro",
+    }
+
 })
